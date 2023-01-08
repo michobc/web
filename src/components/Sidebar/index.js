965c7ec3c1d12212ad8bbaa854ebc6912a7a6f28
micho132 <michelbouchahine132@gmail.com>
2023-01-08 10:12:05 +0200

    added light&dark mode and changed some padding for responsivness on mobiles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,52 +4,64 @@ import myLogo from '../../assets/images/logoM.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome, faUser, faEnvelope, faSuitcase } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { useState } from 'react';
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to='/'>
-            <img src={myLogo} alt="logo" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
+const Sidebar = () => {
+    const [isDarkMode, setIsDarkMode] = useState(false);
 
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
+    const toggleMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
 
-            <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
-                <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-            </NavLink>
+    return (
+        <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
+        <div className='nav-bar'>
+            <Link className='logo' to='/'>
+                <img src={myLogo} alt="logo" />
+            </Link>
+            <button className='flat-button' onClick={toggleMode}>{isDarkMode ? 'Light Mode' : 'Dark Mode'}</button>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
 
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/michel-bou-chahine-97a41419a/">
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://github.com/michobc">
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://www.instagram.com/michel_b.c/?next=%2F">
-                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href="https://twitter.com/Michelbc13">
-                    <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-    </div>
-)
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="portfolio-link" to="/portfolio">
+                    <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+                </NavLink>
+
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://www.linkedin.com/in/michel-bou-chahine-97a41419a/">
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://github.com/michobc">
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://www.instagram.com/michel_b.c/?next=%2F">
+                        <FontAwesomeIcon icon={faInstagram} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href="https://twitter.com/Michelbc13">
+                        <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+        </div>
+    )
+}
 
 export default Sidebar;
